ec720d7d8f1f21ee40ad72404cf66e0f463f7b6e
Chrea Chanchhunneng <47348453+Chhunneng@users.noreply.github.com>
2024-01-05 11:51:18 +0000

    Fri, Jan 5, 2024, 8:51 PM +09:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -62,16 +62,37 @@ int main(int argc, string argv[])
     print_winner();
 }
 
-// Update vote totals given a new vote
 bool vote(string name)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(candidates[i].name, name) == 0)
+        {
+            candidates[i].votes++;
+            return true;
+        }
+    }
+
     return false;
 }
 
-// Print the winner (or winners) of the election
 void print_winner(void)
 {
-    // TODO
-    return;
+    int max_votes = 0;
+
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes > max_votes)
+        {
+            max_votes = candidates[i].votes;
+        }
+    }
+
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes == max_votes)
+        {
+            printf("%s\n", candidates[i].name);
+        }
+    }
 }
