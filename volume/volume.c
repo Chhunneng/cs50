b6ba06fe47a1c88fabf88f7439cbda21cc745e9c
Chrea Chanchhunneng <47348453+Chhunneng@users.noreply.github.com>
2024-01-09 08:45:00 +0000

    Tue, Jan 9, 2024, 5:45 PM +09:00

--- volume/volume.c
+++ volume/volume.c
@@ -9,6 +9,7 @@ const int HEADER_SIZE = 44;
 
 int main(int argc, char *argv[])
 {
+    uint8_t header[HEADER_SIZE];
     // Check command-line arguments
     if (argc != 4)
     {
@@ -34,8 +35,14 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
+    fread(header, HEADER_SIZE, 1, input);
+    fwrite(header, HEADER_SIZE, 1, output);
 
     // TODO: Read samples from input file and write updated data to output file
+    int16_t buffer;
+    fread(&buffer, sizeof(int16_t), 1, input);
+    buffer *= factor;
+    fwrite(&buffer, sizeof(int16_t), 1, output);
 
     // Close files
     fclose(input);
