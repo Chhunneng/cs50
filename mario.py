ef33dd00bc1a6d40913794062b6bca54077cd88d
Chrea Chanchhunneng <47348453+Chhunneng@users.noreply.github.com>
2024-01-10 07:08:13 +0000

    Wed, Jan 10, 2024, 4:08 PM +09:00

--- mario.py
+++ mario.py
@@ -1,5 +1,12 @@
-h = int(input("Height: "))
-while h<1 or h>8:
-    
-for i in range(1,h+1):
-    print(" " * (h-i) + "#" * i)
+h = None
+while True:
+    h = input("Height: ")
+    try:
+        h = int(h)
+        if h >= 1 and h <= 8:
+            break
+    except:
+        pass
+
+for i in range(1, h + 1):
+    print(" " * (h - i) + "#" * i)
