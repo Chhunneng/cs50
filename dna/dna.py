ccfa0be323803c4a84058373bda0f1ba9d10d0bc
Chrea Chanchhunneng <47348453+Chhunneng@users.noreply.github.com>
2024-01-10 07:33:46 +0000

    Wed, Jan 10, 2024, 4:33 PM +09:00

--- dna/dna.py
+++ dna/dna.py
@@ -1,5 +1,12 @@
 import csv
 import sys
+import csv
+
+rows = []
+with open("foo.csv") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        rows.append(row)
 
 
 def main():
@@ -7,7 +14,7 @@ def main():
     # TODO: Check for command-line usage
 
     # TODO: Read database file into a variable
-    
+
     # TODO: Read DNA sequence file into a variable
 
     # TODO: Find longest match of each STR in DNA sequence
@@ -43,11 +50,11 @@ def longest_match(sequence, subsequence):
             # If there is a match in the substring
             if sequence[start:end] == subsequence:
                 count += 1
-            
+
             # If there is no match in the substring
             else:
                 break
-        
+
         # Update most consecutive matches found
         longest_run = max(longest_run, count)
 
