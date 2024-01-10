607da1520cbf87e0b8979468e72e4346cf1cad28
Chrea Chanchhunneng <47348453+Chhunneng@users.noreply.github.com>
2024-01-10 02:39:51 +0000

    Wed, Jan 10, 2024, 11:39 AM +09:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
+typedef unsigned char BYTE;
+
 int main(int argc, char *argv[])
 {
- 
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s image\n", argv[0]);
+        return 1;
+    }
+
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        fprintf(stderr, "Could not open %s for reading.\n", argv[1]);
+        return 1;
+    }
+    BYTE buffer[512];
+
+    // Counter for JPEG filenames
+    int jpeg_count = 0;
+
+    // File pointer for JPEGs
+    FILE *jpeg = NULL;
+
+    // Read the forensic image block by block
+    while (fread(buffer, sizeof(BYTE), 512, file) == 512)
+    {
+        // Check for the start of a new JPEG
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+            // If a JPEG is already open, close it
+            if (jpeg != NULL)
+            {
+                fclose(jpeg);
+            }
+
+            // Create a new JPEG file
+            char filename[8];
+            sprintf(filename, "%03d.jpg", jpeg_count);
+            jpeg = fopen(filename, "w");
+            if (jpeg == NULL)
+            {
+                fprintf(stderr, "Could not create %s.\n", filename);
+                fclose(file);
+                return 1;
+            }
+
+            // Increment the JPEG count
+            jpeg_count++;
+        }
+
+        // Write the block to the current JPEG
+        if (jpeg != NULL)
+        {
+            fwrite(buffer, sizeof(BYTE), 512, jpeg);
+        }
+    }
+
+    // Close any remaining files
+    if (jpeg != NULL)
+    {
+        fclose(jpeg);
+    }
+
+    // Close the forensic image
+    fclose(file);
+
+    return 0;
 }
