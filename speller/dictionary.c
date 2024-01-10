b25f28f276bc8c8fd966b43833c746e2fe91d3f3
Chrea Chanchhunneng <47348453+Chhunneng@users.noreply.github.com>
2024-01-10 06:03:01 +0000

    Wed, Jan 10, 2024, 3:03 PM +09:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -36,14 +36,44 @@ unsigned int hash(const char *word)
 bool load(const char *dictionary)
 {
     FILE *source = fopen(dictionary, "r");
+    if (source == NULL)
+    {
+        fprintf(stderr, "Could not open dictionary.\n");
+        return false;
+    }
 
-    // Read each word in the file
+    // Initialize hash table
+    for (int i = 0; i < N; i++)
+    {
+        table[i] = NULL;
+    }
 
-        // Add each word to the hash table
+    // Read each word in the file and add to the hash table
+    char word[LENGTH + 1];
+    while (fscanf(source, "%s", word) != EOF)
+    {
+        // Allocate memory for a new node
+        node *new_node = malloc(sizeof(node));
+        if (new_node == NULL)
+        {
+            fclose(source);
+            return false;
+        }
+
+        // Copy the word into the new node
+        strcpy(new_node->word, word);
+
+        // Hash the word to obtain its hash value
+        int index = hash(word);
+
+        // Insert the new node into the hash table
+        new_node->next = table[index];
+        table[index] = new_node;
+    }
 
     // Close the dictionary file
     fclose(source);
-    
+    return true;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
