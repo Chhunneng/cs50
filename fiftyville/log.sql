667c930fab501aaa9a523a8e42c8ac93109c9c03
Chrea Chanchhunneng <47348453+Chhunneng@users.noreply.github.com>
2024-01-11 08:09:10 +0000

    Thu, Jan 11, 2024, 5:09 PM +09:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -14,3 +14,18 @@ SELECT account_number, amount
 FROM atm_transactions
 WHERE year = 2023 AND month = 7 AND day = 28 AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw';
 --Now, let's proceed to find the names of the individuals linked to these account numbers. We'll include this information in our suspect list.
+-- Step 4: Find the names associated with the account numbers from the suspect list.
+SELECT name, atm_transactions.account_number, amount
+FROM people
+JOIN bank_accounts ON people.id = bank_accounts.person_id
+JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number
+WHERE atm_transactions.year = 2023 AND atm_transactions.month = 7 AND atm_transactions.day = 28 AND atm_transactions.atm_location = 'Leggett Street' AND atm_transactions.transaction_type = 'withdraw';
+--We now have a list of individuals associated with the ATM transactions on Leggett Street. Bruce seems to be a common name across multiple sources of information. Let's continue building our suspect list and exploring other clues.
+-- Step 5: Continue the investigation with Raymond's clues about the flight.
+-- Find the flights on July 29 from Fiftyville airport, ordering them by time.
+SELECT flights.id, full_name, city, flights.hour, flights.minute
+FROM airports
+JOIN flights ON airports.id = flights.destination_airport_id
+WHERE flights.origin_airport_id = (SELECT id FROM airports WHERE city = 'Fiftyville')
+AND flights.year = 2023 AND flights.month = 7 AND flights.day = 29
+ORDER BY flights.hour, flights.minute;
