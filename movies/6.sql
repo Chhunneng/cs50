e7b1b44d062e8b40466f6e9a99ab4760e6e04cd5
Chrea Chanchhunneng <47348453+Chhunneng@users.noreply.github.com>
2024-01-11 06:36:28 +0000

    Thu, Jan 11, 2024, 3:36 PM +09:00

--- movies/6.sql
+++ movies/6.sql
@@ -1,5 +1,5 @@
 SELECT AVG(rating) FROM movies WHERE year = 2012;
 
 SELECT column0
-FROM table0
-JOIN table1 ON table0.column1 = table1.column2
+FROM ratings
+JOIN movies ON table0.column1 = movies.column2
