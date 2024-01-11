ee865a63a8087da83cd387c7b53b6d814ef49d1d
Chrea Chanchhunneng <47348453+Chhunneng@users.noreply.github.com>
2024-01-11 06:43:24 +0000

    Thu, Jan 11, 2024, 3:43 PM +09:00

--- movies/6.sql
+++ movies/6.sql
@@ -1,3 +1,1 @@
-SELECT AVG(rating) FROM movies WHERE year = 2012;
-
-SELECT AVG(rating) FROM ratings JOIN movies ON ratings.movie_id = movies.id;
+SELECT AVG(rating) FROM ratings JOIN movies ON ratings.movie_id = movies.id where movies.year = 2012;
