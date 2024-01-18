451559ea64d467c124adac11aca0b7606690afff
Chrea Chanchhunneng <47348453+Chhunneng@users.noreply.github.com>
2024-01-18 06:01:38 +0000

    Thu, Jan 18, 2024, 3:01 PM +09:00

--- flash_reg/app.py
+++ flash_reg/app.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        name = request.form.get("name")
+        return render_template("greet.html", name=name)
+    else:
+        return render_template("index.html")
 
 # @app.route("/greet", methods=["POST"])
 # def greet():
