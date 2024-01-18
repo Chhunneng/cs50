e07906b87e08577cb5532fada29e16e11ae4ffd1
Chrea Chanchhunneng <47348453+Chhunneng@users.noreply.github.com>
2024-01-18 06:12:23 +0000

    Thu, Jan 18, 2024, 3:12 PM +09:00

--- flash_reg/app.py
+++ flash_reg/app.py
@@ -2,15 +2,12 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["POST", "GET"])
+@app.route("/")
 def index():
-    if request.method == "POST":
-        name = request.form.get("name")
-        return render_template("greet.html", name=name)
-    else:
-        return render_template("index.html")
+    return render_template("index.html")
 
-# @app.route("/greet", methods=["POST"])
-# def greet():
-    # name = request.form.get("name", "world")
-    # return render_template("greet.html", name=name)
+@app.route("/register", methods=["POST"])
+def greet():
+    if "name" not in request.form
+    name = request.form.get("name", "world")
+    return render_template("greet.html", name=name)
