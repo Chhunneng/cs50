dfcad068d9ddee1b096e172b5a9860f921e8e7a1
Chrea Chanchhunneng <47348453+Chhunneng@users.noreply.github.com>
2024-01-18 06:21:07 +0000

    Thu, Jan 18, 2024, 3:21 PM +09:00

--- flash_reg/app.py
+++ flash_reg/app.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+SPORT = ["Basketball", "Soccer", "Ultimate Frisbee"]
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", sports=SPORT)
 
 @app.route("/register", methods=["POST"])
 def greet():
