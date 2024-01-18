0eb6c766cc6879ddf495794b25d5b81cb63b098e
Chrea Chanchhunneng <47348453+Chhunneng@users.noreply.github.com>
2024-01-18 05:56:55 +0000

    Thu, Jan 18, 2024, 2:56 PM +09:00

---
+++ learn_flask/templates/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST", "GET"])
+def index():
+    if request.method == "POST":
+        name = request.form.get("name")
+        return render_template("greet.html", name=name)
+    else:
+        return render_template("index.html")
+
+# @app.route("/greet", methods=["POST"])
+# def greet():
+    # name = request.form.get("name", "world")
+    # return render_template("greet.html", name=name)
