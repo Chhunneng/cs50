5910c26b9b2faaa2fb27ae729de4260e7b17c316
Chrea Chanchhunneng <47348453+Chhunneng@users.noreply.github.com>
2024-01-18 06:23:55 +0000

    Thu, Jan 18, 2024, 3:23 PM +09:00

--- flash_reg/app.py
+++ flash_reg/app.py
@@ -10,6 +10,6 @@ def index():
 
 @app.route("/register", methods=["POST"])
 def greet():
-    if not request.form.get("name") or request.form.get("sport") not in :
+    if not request.form.get("name") or request.form.get("sport") not in SPORT:
         return render_template("fail.html")
     return render_template("success.html")
